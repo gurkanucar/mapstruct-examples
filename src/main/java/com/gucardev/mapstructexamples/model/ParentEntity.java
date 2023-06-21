7abff4b8f08fdc0dbb55b603ddd2f088dcc52dcd
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-06-22 00:47:24 +0300

    feat: Child and Parent entities created

---
+++ src/main/java/com/gucardev/mapstructexamples/model/ParentEntity.java
@@ -0,0 +1,28 @@
+package com.gucardev.mapstructexamples.model;
+
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "parent")
+public class ParentEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+
+  @OneToMany(mappedBy = "myParent", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<ChildEntity> children;
+}
