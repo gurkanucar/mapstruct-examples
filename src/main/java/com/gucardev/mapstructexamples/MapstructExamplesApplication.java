b067b033ddcdf87c5f05fcd731bcebd274b76623
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-06-21 21:53:46 +0300

    feat: pom.xml dependency added

--- src/main/java/com/gucardev/mapstructexamples/MapstructExamplesApplication.java
+++ src/main/java/com/gucardev/mapstructexamples/MapstructExamplesApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MapstructExamplesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MapstructExamplesApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(MapstructExamplesApplication.class, args);
+  }
 
 }
