92f23faf5152ccfa43012108e9b2e52ddef1f221
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-06-21 21:10:31 +0300

    Initial commit

---
+++ src/main/java/com/gucardev/mapstructexamples/MapstructExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.gucardev.mapstructexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapstructExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapstructExamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gucardev/mapstructexamples/MapstructExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gucardev.mapstructexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapstructExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
